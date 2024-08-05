4c2240659fecf4c65502bb046f3cda89c86e15e6
riwoo0902 <imliu4342@gmail.com>
2024-08-05 19:59:08 +0900

    배경추가 배경 움직임효과추가

--- src/SpaceWar.py
+++ src/SpaceWar.py
@@ -4,14 +4,18 @@ from pygame.locals import *
 
 class SpaceWar():
     run = True
-    WIDTH = 400
-    HEIGHT = 400
+    WIDTH = 1500
+    HEIGHT = 800
+    backgrondxy = [0,0]
+    backgrondxy2 = [-WIDTH,0]
     def __init__(self) -> None:
         pygame.init()
         pygame.display.set_caption("SpaceWar")
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT)) 
-
+        self.backgrond = pygame.image.load('./images/background.png').convert_alpha()
+        self.backgrond1 = pygame.transform.scale(self.backgrond, (self.WIDTH,self.HEIGHT))
+        self.backgrond2 = pygame.transform.scale(self.backgrond, (self.WIDTH,self.HEIGHT))
 
     #이벤트 확인 및 처리 함수
     def eventkey(self):
@@ -25,12 +29,24 @@ class SpaceWar():
                 if event.key == pygame.K_SPACE:
                     pass
                 
+    def backgrondmove(self):
+        self.backgrondxy[0] += 0.5
+        self.backgrondxy2[0] += 0.5
+        if self.backgrondxy[0] == self.WIDTH:
+            self.backgrondxy = [0,0]
+            self.backgrondxy2 = [-self.WIDTH,0]
+        self.screen.blit(self.backgrond1, self.backgrondxy)
+        self.screen.blit(self.backgrond2, self.backgrondxy2)
     def loop(self):
         while self.run:
             self.screen.fill((255, 255, 255)) 
             self.eventkey() 
+            self.backgrondmove()
             pygame.display.update() 
-            self.clock.tick(60) 
-            
+            self.clock.tick(400) 
+    
+    
+        
+        
 main = SpaceWar()
 main.loop()
